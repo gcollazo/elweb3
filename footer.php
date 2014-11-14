a0fcc01ff158549a2b13fd03af52b7785b275c03
Giovanni Collazo <hello@gcollazo.com>
2014-11-14 15:47:08 -0400

    Remove http protocol

--- footer.php
+++ footer.php
@@ -20,13 +20,8 @@
 
         <?php wp_footer(); ?>
 
-        <script src="<?php bloginfo('template_directory'); ?>/js/vendor.js"></script>
+        <!-- <script src="<?php bloginfo('template_directory'); ?>/js/vendor.js"></script> -->
         <!-- <script src="<?php bloginfo('template_directory'); ?>/js/app.js"></script> -->
         <!-- <script>require('elweb');</script> -->
-        <!--[if !(lte IE 8)]><!-->
-           <script type="text/javascript">
-             (function(){var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src=document.location.protocol+"//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})()
-           </script>
-        <!--<![endif]-->
     </body>
 </html>
--- social.php
+++ social.php
@@ -5,8 +5,8 @@ if (!defined('ABSPATH')) exit;
 ?>
 <div class="row social-buttons-wrapper">
     <ul class="social-buttons-list">
-        <li class="social-button-item social-button-twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="<?php echo $GLOBALS['_DEFAULT_TWITTER_USERNAME']; ?>">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></li>
-        <li class="social-button-item social-button-plus"><g:plusone size="medium" style="display:inline"></g:plusone><script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script></li>
-        <li class="social-button-item social-button-facebook"><iframe src="http://www.facebook.com/plugins/like.php?app_id=<?php echo $GLOBALS['_DEFAULT_FACEBOOK_APP_ID']; ?>&amp;href=<?php the_permalink() ?>&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe></li>
+        <li class="social-button-item social-button-twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="<?php echo $GLOBALS['_DEFAULT_TWITTER_USERNAME']; ?>">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script></li>
+        <li class="social-button-item social-button-plus"><g:plusone size="medium" style="display:inline"></g:plusone><script type="text/javascript" src="//apis.google.com/js/plusone.js"></script></li>
+        <li class="social-button-item social-button-facebook"><iframe src="//www.facebook.com/plugins/like.php?app_id=<?php echo $GLOBALS['_DEFAULT_FACEBOOK_APP_ID']; ?>&amp;href=<?php the_permalink() ?>&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe></li>
     </ul> <!-- .social-buttons-list -->
 </div> <!-- .social-buttons-wrapper -->
