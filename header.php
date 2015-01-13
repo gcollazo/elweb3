ac21fef5240bf9402eba9adb059c7e44de56e896
Giovanni Collazo <hello@gcollazo.com>
2015-01-13 08:54:46 -0400

    Change how titles work

--- header.php
+++ header.php
@@ -21,7 +21,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title('&mdash;', 1, 'right'); ?><?php bloginfo('name'); ?></title>
+    <?php if(is_home()):?>
+        <title><?php bloginfo('name'); ?></title>
+    <?php else: ?>
+        <title><?php wp_title('', 1); ?></title>
+    <?php endif; ?>
 
     <meta name="description" content="<?php if (have_posts() && !is_home()): while (have_posts()): the_post(); echo strip_tags(get_the_excerpt()); endwhile; else: echo $GLOBALS['_DEFAULT_BLOG_DESCRIPTION']; endif;?>" />
 
